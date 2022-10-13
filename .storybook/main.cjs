dfcf4945a08dab0c92a9339b11ccd0f7719b202e
Alex Silva <alexcssilva@gmail.com>
2022-10-13 17:20:12 -0300

    cy: Added viteFinal main.cjs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/Ignite-Lab-Design-System/'
+      config.base = '/Ignite-Lab-Design-System'
     }
 
     return config
