325881a22e11e280dec934eff6e0c12b50dc59a0
Alex Silva <alexcssilva@gmail.com>
2022-10-13 13:34:20 -0300

    feat: added initial commit

---
+++ storybook-static/assets/preview.8f1c584b.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.c00f40f4.js";import"./iframe.d6737c34.js";import"./es.map.constructor.81698393.js";import"./es.number.to-fixed.19b82a3c.js";import"./index.631a4785.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.8f1c584b.js.map
